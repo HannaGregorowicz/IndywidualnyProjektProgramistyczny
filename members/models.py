249ac565a258f8f54ae73ff91e7a96adec67e105
HannaGregorowicz <33255939+HannaGregorowicz@users.noreply.github.com>
2019-04-13 21:33:31 +0200

    Initial commit

---
+++ members/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Member(models.Model):
+    name = models.CharField(max_length=200)
+    technologies = models.CharField(max_length=500)
+    photo = models.FileField()
+    more = models.TextField()
+
---
+++ projects/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Project(models.Model):
+    title = models.CharField(max_length=500)
+    description = models.TextField()
+    photo = models.FileField(null=True)
+    date = models.DateField(auto_now_add=True)
