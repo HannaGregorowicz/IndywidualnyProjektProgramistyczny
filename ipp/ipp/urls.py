18796910785c8a510264d3f92083e61f94265088
HannaGregorowicz <33255939+HannaGregorowicz@users.noreply.github.com>
2019-05-31 00:15:35 +0200

    files

--- ipp/ipp/urls.py
+++ ipp/ipp/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^', include('about.urls')),
@@ -9,3 +11,7 @@ urlpatterns = [
     url(r'^members/', include('members.urls')),
     url(r'^projects/', include('projects.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
